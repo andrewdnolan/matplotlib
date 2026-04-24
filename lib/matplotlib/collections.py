c2bd0d7a41e7e343f7d9ce5f8fae767db71d1e5b
Andrew Nolan <anolan@lanl.gov>
2026-04-24 17:23:35 -0600

    Add PolyMeshCollection with custom draw method

--- lib/matplotlib/collections.py
+++ lib/matplotlib/collections.py
@@ -1378,6 +1378,95 @@ class PolyCollection(_CollectionWithSizes):
         self.stale = True
 
 
+class PolyMeshCollection(PolyCollection):
+    r"""
+    Class for more efficient drawing of a `.PolyCollection`
+
+    The polygons will be drawn as an structure of arrays, instead of an array
+    of structures. Analogous to `.QuadMesh` vs. `.PolyQuadMesh`
+
+    Parameters
+    ----------
+    verts : list of array-like
+        The sequence of polygons [*verts0*, *verts1*, ...] where each
+        element *verts_i* defines the vertices of polygon *i* as a 2D
+        array-like of shape (M, 2).
+
+    antialiased : bool, default: True
+
+    Notes
+    -----
+    Like `.QuadMesh` the default *pickradius* is 0, i.e. `~.Artist.contains`
+    checks whether the test point is within any of the mesh polygons.
+    """
+
+    def __init__(self, verts, antialiased=True, **kwargs):
+        kwargs.setdefault("pickradius", 0)
+        super().__init__(verts=verts, **kwargs)
+        self._bbox = transforms.Bbox.unit()
+        self._bbox.update_from_data_xy(verts.reshape(-1, 2))
+        self._antialiased = antialiased
+        self.set_mouseover(False)
+
+        self._coordinates = np.ascontiguousarray(verts)
+
+    @artist.allow_rasterization
+    def draw(self, renderer):
+        if not self.get_visible():
+            return
+        renderer.open_group(self.__class__.__name__, self.get_gid())
+        transform = self.get_transform()
+        offset_trf = self.get_offset_transform()
+        offsets = self.get_offsets()
+
+        if self.have_units():
+            xs = self.convert_xunits(offsets[:, 0])
+            ys = self.convert_yunits(offsets[:, 1])
+            offsets = np.column_stack([xs, ys])
+
+        self.update_scalarmappable()
+
+        if not transform.is_affine:
+            coordinates = self._coordinates.reshape((-1, 2))
+            coordinates = transform.transform(coordinates)
+            coordinates = coordinates.reshape(self._coordinates.shape)
+            transform = transforms.IdentityTransform()
+        else:
+            coordinates = self._coordinates
+
+        if not offset_trf.is_affine:
+            offsets = offset_trf.transform_non_affine(offsets)
+            offset_trf = offset_trf.get_affine()
+
+        gc = renderer.new_gc()
+        gc.set_snap(self.get_snap())
+        self._set_gc_clip(gc)
+        gc.set_linewidth(self.get_linewidth()[0])
+
+        renderer.draw_poly_mesh(
+            gc,
+            transform.frozen(),
+            coordinates.shape[1],
+            coordinates.shape[0],
+            coordinates,
+            offsets,
+            offset_trf,
+            self.get_facecolor().reshape((-1, 4)),
+            self._antialiased,
+            self.get_edgecolors().reshape((-1, 4)),
+        )
+
+        gc.restore()
+        renderer.close_group(self.__class__.__name__)
+        self.stale = False
+
+    def get_cursor_data(self, event):
+        contained, info = self.contains(event)
+        if contained and self.get_array() is not None:
+            return self.get_array().ravel()[info["ind"]]
+        return None
+
+
 class FillBetweenPolyCollection(PolyCollection):
     """
     `.PolyCollection` that fills the area between two x- or y-curves.
